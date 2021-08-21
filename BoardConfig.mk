f679750844729883ab733a6508e1b2b887dabb35
Foxtrot47 <jjneutron@outlook.com>
2021-08-21 10:38:49 +0530

    vayu: Switch Defconfig and use lld

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,8 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_vayu
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_vayu
 
 # Kernel
-TARGET_KERNEL_CONFIG := vendor/vayu_defconfig
+TARGET_KERNEL_CONFIG := vayu_user_defconfig
+TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld
 
 # Inherit from the proprietary version
 include vendor/xiaomi/vayu/BoardConfigVendor.mk
